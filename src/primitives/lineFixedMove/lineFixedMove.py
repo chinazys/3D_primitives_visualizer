4bfd2efdd9449c9321388cff06dd6b1298b9dc51
EugenTheMachine <105974307+EugenTheMachine@users.noreply.github.com>
2023-04-26 21:32:58 +0300

    improved animation + cleaned up garbage

--- src/primitives/lineFixedMove/lineFixedMove.py
+++ src/primitives/lineFixedMove/lineFixedMove.py
@@ -2,19 +2,19 @@ from primitives.curve.curve import Curve
 from primitives.primitive import Primitive
 import numpy as np
 from matplotlib.animation import FuncAnimation
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+# import matplotlib.pyplot as plt
+# from PyQt5.QtCore import QTimer
+# from time import sleep
+# import time
 
 # temporay, this function is not used anywhere below
-def boundCoordinates(fixdot, dot):
-    x0, y0, z0 = fixdot
-    #print(xyz)
-    x1, y1, z1 = dot
-    #v = [xyz[0] - x1, xyz[1] - y1, xyz[2] - z1]
-    v = [x0 - x1, y0 - y1, z0 - z1]
-    k = 0.1
+# def boundCoordinates(fixdot, dot):
+#     x0, y0, z0 = fixdot
+#     x1, y1, z1 = dot
+#     v = [x0 - x1, y0 - y1, z0 - z1]
+#     k = 0.1
 
-    return [[x0+v[0]*k, x1-v[0]*k], [y0+v[1]*k, y1-v[1]*k], [z0+v[2]*k, z1-v[2]*k]]
+#     return [[x0+v[0]*k, x1-v[0]*k], [y0+v[1]*k, y1-v[1]*k], [z0+v[2]*k, z1-v[2]*k]]
 
 # class defining fixed move of a line
 # 2 input params: curve 'base' and fixation dot 'fixdot'
@@ -30,6 +30,7 @@ class lineFixedMove(Primitive):
         self.tmp_y = []
         self.tmp_z = []
         self.surfaces = []
+        self.surf = None
         
         # GENERAL PARAMETERS: are applied when plotting both animated and result surfaces
         self.ALPHA = .75  # sets capacity level
@@ -41,31 +42,40 @@ class lineFixedMove(Primitive):
         self.STEP = self.DOTS//self.LENGTH  # calculates the step of taking next dot to expand the animated conical surface
         self.TMP_RESOLUTION = 2  # number of dots each line from curve to fixdot contains
         self.PAUSE = 1  # sets pause time in seconds between fixdot, base curve and the surface plots
-        self.INTERVAL = .1
-        # print(self.DOTS)
+        self.INTERVAL = 50
+        # bias is required because during first several frames we build fixdot, base curve, P dot and connecting line
+        self.BIAS = 4
 
     def build(self):
         pass
     
     # Updated plot() function with implemented animation
     def plot(self, ax, canvas, fig):
-        try:
+        try:            
             # defined function for creating i-th frame of animation
             def animate(i):
-                # firstly, plot the fixdot and the base Curve
-                if i == 0:
+                # firstly, plot the fixdot
+                if i == 0:                    
+                    ax.scatter(*self.fixdot, color='red', s=40)
+                    return
+                # next, plot the base curve                
+                elif i == 1:
+                    ax.plot(self.base.x_list, self.base.y_list, self.base.z_list, color='green', linewidth=5)
+                    return
+                # then, plot P dot (the first base-curve dot in the list)
+                elif i == 2:
+                    x1, y1, z1 = self.base.x_list[0], self.base.y_list[0], self.base.z_list[0]
+                    ax.scatter(x1, y1, z1, color='red', s=40)
+                    return
+                # finally, plot connecting line between P dot and fixdot
+                elif i == 3:
                     x0, y0, z0 = self.fixdot
                     x1, y1, z1 = self.base.x_list[0], self.base.y_list[0], self.base.z_list[0]
-                    ax.scatter(*self.fixdot, color='red', s=40)
-                    ax.text(x0, y0, z0+10, "Fixdot", fontsize=15, ha='center', va='center')
-                    # plt.pause(self.PAUSE)
-                    ax.plot(self.base.x_list, self.base.y_list, self.base.z_list, color='green', linewidth=5)
-                    ax.text(x1, y1, z1-10, "Base", fontsize=15, ha='center', va='center')
-                    # plt.pause(self.PAUSE)
                     ax.plot([x0, x1], [y0, y1], [z0, z1], color='blue', linewidth=5)
-                    line = ax.text((x0+x1)/2+10, (y0+y1)/2+10, (z0+z1)/2+10, "Line", fontsize=15)
+                    self.INTERVAL = 100
+                    return
                 # if it's the last dot, unite all the fragments into single surface
-                if i*(self.STEP) >= self.DOTS-1:
+                if (i-self.BIAS)*(self.STEP) >= self.DOTS-1:
                     x0, y0, z0 = self.fixdot
                     xx = np.linspace(x0, self.base.x_list[self.DOTS-1], self.TMP_RESOLUTION)
                     yy = np.linspace(y0, self.base.y_list[self.DOTS-1], self.TMP_RESOLUTION)
@@ -79,7 +89,7 @@ class lineFixedMove(Primitive):
                     X = np.array(self._x)
                     Y = np.array(self._y)
                     Z = np.array(self._z)
-                    surf = ax.plot_surface(X, Y, Z, color=self.COLOR, alpha=self.ALPHA)
+                    surf = ax.plot_surface(X, Y, Z, color=self.COLOR, alpha=self.ALPHA, picker=False)
                     self.surfaces.append(surf)
                     self._x.clear()
                     self._y.clear()
@@ -87,16 +97,13 @@ class lineFixedMove(Primitive):
                     X = np.array(self.tmp_x)
                     Y = np.array(self.tmp_y)
                     Z = np.array(self.tmp_z)
-                    plt.pause(self.PAUSE)
                     for s in self.surfaces:
                         s.remove()
-                    plt.pause(self.PAUSE)
-                    ax.plot_surface(X, Y, Z, alpha=self.ALPHA, color=self.COLOR)
-                    # print("FINISH")
-                    return
-                
+                    self.surf = ax.plot_surface(X, Y, Z, alpha=self.ALPHA, color=self.COLOR, picker=True)
+                    return True                
                 # usually, we just add another set of dots to the list
                 else:
+                    i -= self.BIAS
                     x0, y0, z0 = self.fixdot
                     xx = np.linspace(x0, self.base.x_list[i*self.STEP], self.TMP_RESOLUTION)
                     yy = np.linspace(y0, self.base.y_list[i*self.STEP], self.TMP_RESOLUTION)
@@ -120,11 +127,8 @@ class lineFixedMove(Primitive):
                     self._z.clear()
                     self._x.append(tmp[0])
                     self._y.append(tmp[1])
-                    self._z.append(tmp[2])
-
-            # create the animation object itself and assign it to variable 'anim'           
-            anim = FuncAnimation(fig, animate, frames=self.LENGTH+1, repeat=False, interval=self.INTERVAL)
-
-            canvas.draw()
+                    self._z.append(tmp[2])                     
+            anim = FuncAnimation(fig, animate, frames=self.LENGTH+1+self.BIAS, repeat=False, interval=self.INTERVAL)
+            canvas.draw()            
         except:
             print('Surface is invalid => cannot plot')
