c0add072316b6b73103078dee9760e8cd671f545
Alex <7alex0041@gmail.com>
2023-04-30 01:07:34 +0200

    Change roatte_metod rotate_surface.py

--- src/primitives/rotate_surface/rotate_surface.py
+++ src/primitives/rotate_surface/rotate_surface.py
@@ -20,56 +20,61 @@ class rotate_surface(Primitive):
 
         self.vector = vector
 
-        #self.main_line = Line([dot, vector + dot])
+        # self.main_line = Line([dot, vector + dot])
 
-        #self.main_line.build()
+        # self.main_line.build()
 
         self.x_list, self.y_list, self.z_list = [], [], []
 
     def build(self):
 
-        def rotate_around_line(point, line_point, line_direction, angle):
-
-            shifted_point = point - line_point
-
-            direction = line_direction / np.linalg.norm(line_direction)
-
-            v = np.cross([0, 0, 1], direction)
-            c = np.dot([0, 0, 1], direction)
-            s = np.linalg.norm(v)
-            vx = np.array([[0, -v[2], v[1]], [v[2], 0, -v[0]], [-v[1], v[0], 0]])
-            rotation_matrix = np.eye(3) + vx + np.dot(vx, vx) * ((1 - c) / s ** 2)
-
+        def rotate_point_about_line(point, line_point, line_direction, angle):
+            # Convert angle to radians
             theta = np.radians(angle)
+
+            # Convert all inputs to numpy arrays for consistency
+            point = np.array(point)
+            line_point = np.array(line_point)
+            line_direction = np.array(line_direction)
+
+            # Calculate unit direction vector
+            u = line_direction / np.linalg.norm(line_direction)
+
+            # Calculate rotation matrix
             cos_theta = np.cos(theta)
             sin_theta = np.sin(theta)
-            rotation_matrix_z = np.array([[cos_theta, -sin_theta, 0], [sin_theta, cos_theta, 0], [0, 0, 1]])
-            rotated_point = np.dot(rotation_matrix_z, np.dot(rotation_matrix, shifted_point))
+            u_x = u[0]
+            u_y = u[1]
+            u_z = u[2]
+            rot_mat = np.array([[cos_theta + u_x ** 2 * (1 - cos_theta), u_x * u_y * (1 - cos_theta) - u_z * sin_theta,
+                                 u_x * u_z * (1 - cos_theta) + u_y * sin_theta],
+                                [u_y * u_x * (1 - cos_theta) + u_z * sin_theta, cos_theta + u_y ** 2 * (1 - cos_theta),
+                                 u_y * u_z * (1 - cos_theta) - u_x * sin_theta],
+                                [u_z * u_x * (1 - cos_theta) - u_y * sin_theta,
+                                 u_z * u_y * (1 - cos_theta) + u_x * sin_theta,
+                                 cos_theta + u_z ** 2 * (1 - cos_theta)]])
 
-            rotated_point = np.dot(np.linalg.inv(rotation_matrix), rotated_point)
+            # Apply rotation to point
+            rotated_point = np.matmul(rot_mat, (point - line_point)) + line_point
 
-            rotated_point += line_point
-
-            return rotated_point
-
-
+            return rotated_point.tolist()
 
         a, b, c = self.dot
         m, n, p = self.vector
-        #ax=self.main_line.plot(ax)
+
+        # ax=self.main_line.plot(ax)
 
         x = self.base.x_list
         y = self.base.y_list
         z = self.base.z_list
 
-
-
         x_new, y_new, z_new = [], [], []
         x_, y_, z_ = [], [], []
-        for i in range(len(x)):
-            for j in range(0, 366,36):
+
+        for j in range(0, 366):
+            for i in range(0, len(x)):
                 theta = j
-                x_rot, y_rot, z_rot = rotate_around_line(np.array([x[i], y[i], z[i]]), np.array([a, b, c]),
+                x_rot, y_rot, z_rot = rotate_point_about_line(np.array([x[i], y[i], z[i]]), np.array([a, b, c]),
                                                          np.array([m, n, p]), theta)
                 x_new.append(x_rot)
                 y_new.append(y_rot)
@@ -80,32 +85,28 @@ class rotate_surface(Primitive):
             x_new, y_new, z_new = [], [], []
 
 
+
         self.x_list = np.array(x_)
         self.y_list = np.array(y_)
         self.z_list = np.array(z_)
 
-
-
-
     def plot(self, ax, canvas, fig):
         from matplotlib.animation import FuncAnimation
 
-        #self.main_line.plot()
-        #self.base.plot()
+        # self.main_line.plot()
+        # self.base.plot()
 
+        # self.build()
 
-        #self.build()
-
-        t = np.linspace(-5, 5, 100)
+        t = np.linspace(-50, 50, 100)
         a, b, c = self.dot
         m, n, p = self.vector
         x_line = a + m * t
         y_line = b + n * t
         z_line = c + p * t
         ax.plot(x_line, y_line, z_line)
-        # print(self.x_list,self.z_list,self.y_list)
         ax.plot(self.base.x_list, self.base.y_list, self.base.z_list)
         ax.plot_surface(self.x_list, self.y_list, self.z_list, color='b')
-        #anim = FuncAnimation(fig, animate, frames=self.LENGTH + 1, repeat=False, interval=self.INTERVAL)
+        # anim = FuncAnimation(fig, animate, frames=self.LENGTH + 1, repeat=False, interval=self.INTERVAL)
 
         canvas.draw()
