5e42cddf10b0f713ae442c56db4c60535eddc62d
EugenTheMachine <105974307+EugenTheMachine@users.noreply.github.com>
2023-05-29 09:59:15 +0300

    edited plane building function

--- src/primitives/plane/plane.py
+++ src/primitives/plane/plane.py
@@ -34,17 +34,34 @@ class Plane(Primitive):
     def _fill_points_list(self):
         self.x_list = np.linspace(-100, 100, 2)
         self.y_list = np.linspace(-100, 100, 2)
-        self.X_list, self.Y_list = np.meshgrid(self.x_list, self.y_list)
+        self.z_list = np.linspace(-100, 100, 2)
 
         m1, m2, m3 = self.M.x, self.M.y, self.M.z
         n1, n2, n3 = self.n.x, self.n.y, self.n.z
-        
-        self.Z_list = (-n1*self.X_list+n1*m1-n2*self.Y_list+n2*m2+n3*m3) / n3
 
-        self.x_saved = np.linspace(-100, 100, 200)
-        self.y_saved = np.linspace(-100, 100, 200)
-        self.X_saved, self.Y_saved = np.meshgrid(self.x_saved, self.y_saved)
-        self.Z_saved = (-n1*self.X_saved+n1*m1-n2*self.Y_saved+n2*m2+n3*m3) / n3
+        if n3 != 0:
+            self.X_list, self.Y_list = np.meshgrid(self.x_list, self.y_list)
+            self.Z_list = (-n1*self.X_list+n1*m1-n2*self.Y_list+n2*m2) / n3 + m3
+            self.x_saved = np.linspace(-100, 100, 200)
+            self.y_saved = np.linspace(-100, 100, 200)
+            self.X_saved, self.Y_saved = np.meshgrid(self.x_saved, self.y_saved)
+            self.Z_saved = (-n1*self.X_saved+n1*m1-n2*self.Y_saved+n2*m2+n3*m3) / n3
+        elif n2 != 0:
+            self.X_list, self.Z_list = np.meshgrid(self.x_list, self.z_list)
+            self.Y_list = (-n1*self.X_list+n1*m1-n3*self.Z_list+n3*m3) / n2 + m2
+            self.x_saved = np.linspace(-100, 100, 200)
+            self.z_saved = np.linspace(-100, 100, 200)
+            self.X_saved, self.Z_saved = np.meshgrid(self.x_saved, self.z_saved)
+            self.Y_saved = (-n1*self.X_saved+n1*m1-n3*self.Z_saved+n3*m3) / n2 + m2
+        elif n1 != 0:
+            self.Y_list, self.Z_list = np.meshgrid(self.y_list, self.z_list)
+            self.X_list = (-n2*self.Y_list+n2*m2-n3*self.Z_list+n3*m3) / n1 + m1
+            self.y_saved = np.linspace(-100, 100, 200)
+            self.z_saved = np.linspace(-100, 100, 200)
+            self.Y_saved, self.Z_saved = np.meshgrid(self.y_saved, self.z_saved)
+            self.X_saved = (-n2*self.Y_saved+n2*m2-n3*self.Z_saved+n3*m3) / n1 + m1
+        else:
+            raise Exception("n vector = (0,0,0)!")
 
 
     def build(self):
